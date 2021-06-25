83062d144b88cd29f174ddca04032b97874e3a38
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-25 17:54:42 +0530

    Created Angular Employee List Component

---
+++ angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Employee} from "../employee"
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.employees = [
+      {
+          "id": 1,
+          "firstName": "Ross",
+          "lastName": "Geller",
+          "emailId": "ross@gmail.com"
+      },
+      {
+          "id": 2,
+          "firstName": "Phoebe",
+          "lastName": "Buffay",
+          "emailId": "phoebe@gmail.com"
+      }
+  ]
+  }
+
+}
